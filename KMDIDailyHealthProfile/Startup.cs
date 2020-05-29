57cbecc98b523c080ce1ff9bfcf6172a1139a6a4
warren222 <warrenmangaring22@gmail.com>
2020-05-29 09:18:13 +0800

    Add project files.

---
+++ KMDIDailyHealthProfile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KMDIDailyHealthProfile.Startup))]
+namespace KMDIDailyHealthProfile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
