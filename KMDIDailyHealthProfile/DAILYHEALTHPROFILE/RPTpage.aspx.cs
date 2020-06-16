bcb3ce4d584e066a0d5f9e7721c0d92f1d6fa074
warren222 <warrenmangaring22@gmail.com>
2020-06-17 07:53:46 +0800

    june 17, 2020

--- KMDIDailyHealthProfile/DAILYHEALTHPROFILE/RPTpage.aspx.cs
+++ KMDIDailyHealthProfile/DAILYHEALTHPROFILE/RPTpage.aspx.cs
@@ -18,14 +18,21 @@ namespace KMDIDailyHealthProfile.DAILYHEALTHPROFILE
 
                 if (!IsPostBack)
                 {
-
+                    ReportViewer1.LocalReport.EnableExternalImages = true;
                     string filepath = "~/Uploads/DHPuploads/page2/signature/patient/" + empno + dhpid + "/";
+
+                    Boolean IsExists = System.IO.Directory.Exists(Server.MapPath(filepath));
+                    if (!IsExists)
+                    {
+                        System.IO.Directory.CreateDirectory(Server.MapPath(filepath));
+                    }
+
 
                     foreach (string strfilename in Directory.GetFiles(Server.MapPath(filepath)))
                     {
 
                         FileInfo fileinfo = new FileInfo(strfilename);
-                        ReportViewer1.LocalReport.EnableExternalImages = true;
+                      
                         string prepared = new Uri(Server.MapPath("~/Uploads/DHPuploads/page2/signature/patient/" + empno + dhpid + "/" + fileinfo.Name)).AbsoluteUri;
                         ReportParameter param1 = new ReportParameter("patientsignature", prepared);
                         ReportViewer1.LocalReport.SetParameters(param1);
